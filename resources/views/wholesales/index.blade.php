393e7c56fb0dc934f8d9b87368144410ce92bd71
Deki Kurnia <dekikurnia@gmail.com>
2023-03-14 06:29:02 +0700

    Enable/Disable ubah tangggal sesuai role pada entry penjualan grosir

--- resources/views/wholesales/index.blade.php
+++ resources/views/wholesales/index.blade.php
@@ -16,7 +16,11 @@
                         </div>
                         <div class="form-group col-md-10">
                             <label for="tanggal">Tanggal</label>
+                            @hasanyrole('Technician|Admin')
+                            <input type="text" value="{{ date('Y-m-d') }}" class="form-control" name="tanggal" autocomplete="off">
+                            @else
                             <input type="text" value="{{ date('Y-m-d') }}" class="form-control" name="tanggal" autocomplete="off" readonly>
+                            @endhasanyrole
                         </div>
                         <div class="form-group col-md-10">
                             <label for="customer">Customer</label>
